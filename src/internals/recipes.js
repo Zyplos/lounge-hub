128af3f07360121ac612d2c5e52c27a342acf139
Angel <22824872+Zyplos@users.noreply.github.com>
2020-07-07 12:59:08 -0500

    VanillaTweaks recipes

--- src/internals/recipes.js
+++ src/internals/recipes.js
@@ -5,6 +5,29 @@ import greenDye from "../assets/items/green_dye.png";
 import rottenFlesh from "../assets/items/rotten_flesh.png";
 import leather from "../assets/items/leather.png";
 import cookedMutton from "../assets/items/cooked_mutton.png";
+import stick from "../assets/items/stick.png";
+import fireCharge from "../assets/items/fire_charge.png";
+import soulTorch from "../assets/items/soul_torch.png";
+import charcoal from "../assets/items/charcoal.png";
+import anyDye from "../assets/items/dye.gif";
+import redDye from "../assets/items/red_dye.png";
+import paper from "../assets/items/paper.png";
+import string from "../assets/items/string.png";
+import ironIngot from "../assets/items/iron_ingot.png";
+import redstone from "../assets/items/redstone.png";
+import nameTag from "../assets/items/name_tag.png";
+import apple from "../assets/items/apple.png";
+import enchantedGoldenApple from "../assets/items/enchanted_golden_apple.gif";
+
+import stonePickaxe from "../assets/items/stone_pickaxe.png";
+import stoneAxe from "../assets/items/stone_axe.png";
+import stoneShovel from "../assets/items/stone_shovel.png";
+import stoneHoe from "../assets/items/stone_hoe.png";
+import stoneSword from "../assets/items/stone_sword.png";
+import blazeRod from "../assets/items/blaze_rod.png";
+import brewingStand from "../assets/items/brewing_stand.png";
+import bow from "../assets/items/bow.png";
+import lever from "../assets/items/lever.png";
 
 import stone from "../assets/items/stone.png";
 import cobblestone from "../assets/items/cobblestone.png";
@@ -14,9 +37,30 @@ import granite from "../assets/items/granite.png";
 import polishedAndesite from "../assets/items/polished_andesite.png";
 import polishedDiorite from "../assets/items/polished_diorite.png";
 import polishedGranite from "../assets/items/polished_granite.png";
+import blackstone from "../assets/items/blackstone.png";
+
+import blockOfGold from "../assets/items/block_of_gold.png";
+import furnace from "../assets/items/furnace.png";
+import piston from "../assets/items/piston.png";
+import observer from "../assets/items/observer.png";
+import dispenser from "../assets/items/dispenser.png";
+import dropper from "../assets/items/dropper.png";
 
 import glass from "../assets/items/glass.png";
 import sand from "../assets/items/sand.png";
+import sandstone from "../assets/items/sandstone.png";
+import smoothSandstone from "../assets/items/smooth_sandstone.png";
+import redSand from "../assets/items/red_sand.png";
+import redSandstone from "../assets/items/red_sandstone.png";
+import smoothRedSandstone from "../assets/items/smooth_red_sandstone.png";
+import soulSoil from "../assets/items/soul_soil.png";
+import soulSand from "../assets/items/soul_sand.png";
+import anyBed from "../assets/items/bed.gif";
+import anyConcretePowder from "../assets/items/concrete_powder.gif";
+import anyGlazedTerracotta from "../assets/items/glazed_terracotta.gif";
+import anyStainedGlass from "../assets/items/stained_glass.gif";
+import anyStainedGlassPane from "../assets/items/stained_glass_pane.gif";
+import anyWool from "../assets/items/wool.gif";
 
 import oakLog from "../assets/items/oak_log.png";
 import birchLog from "../assets/items/birch_log.png";
@@ -26,6 +70,8 @@ import acaciaLog from "../assets/items/acacia_log.png";
 import darkOakLog from "../assets/items/dark_oak_log.png";
 import crimsonStem from "../assets/items/crimson_stem.gif";
 import warpedStem from "../assets/items/warped_stem.gif";
+
+import oakPlanks from "../assets/items/oak_planks.png";
 
 import strippedOakLog from "../assets/items/stripped_oak_log.png";
 import strippedBirchLog from "../assets/items/stripped_birch_log.png";
@@ -239,5 +285,440 @@ export default [
     type: "blasting",
     input: ["Sand", sand],
     result: ["Glass", glass],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Fire Charge", fireCharge],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Soul Soil", soulSoil],
+      false,
+    ],
+    result: ["Soul Torch", soulTorch, 4],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Fire Charge", fireCharge],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Soul Sand", soulSand],
+      false,
+    ],
+    result: ["Soul Torch", soulTorch, 4],
+  },
+  {
+    type: "blasting",
+    input: ["Jungle Log", jungleLog],
+    result: ["Charcoal", charcoal],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Bed", anyBed],
+      ["Any Dye", anyDye],
+      ["Any Dye", anyDye],
+
+      ["Any Dye", anyDye],
+      false,
+      false,
+
+      false,
+      false,
+      false,
+    ],
+    result: ["Any Bed", anyBed],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Concrete Powder", anyConcretePowder],
+      ["Any Concrete Powder", anyConcretePowder],
+      ["Any Concrete Powder", anyConcretePowder],
+
+      ["Any Concrete Powder", anyConcretePowder],
+      ["Any Dye", anyDye],
+      ["Any Concrete Powder", anyConcretePowder],
+
+      ["Any Concrete Powder", anyConcretePowder],
+      ["Any Concrete Powder", anyConcretePowder],
+      ["Any Concrete Powder", anyConcretePowder],
+    ],
+    result: ["Any Concrete Powder", anyConcretePowder, 8],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+      ["Any Dye", anyDye],
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+      ["Any Glazed Terracotta", anyGlazedTerracotta],
+    ],
+    result: ["Any Glazed Terracotta", anyGlazedTerracotta, 8],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Stained Glass", anyStainedGlass],
+      ["Any Stained Glass", anyStainedGlass],
+      ["Any Stained Glass", anyStainedGlass],
+
+      ["Any Stained Glass", anyStainedGlass],
+      ["Any Dye", anyDye],
+      ["Any Stained Glass", anyStainedGlass],
+
+      ["Any Stained Glass", anyStainedGlass],
+      ["Any Stained Glass", anyStainedGlass],
+      ["Any Stained Glass", anyStainedGlass],
+    ],
+    result: ["Any Stained Glass", anyStainedGlass, 8],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+      ["Any Dye", anyDye],
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+      ["Any Stained Glass Pane", anyStainedGlassPane],
+    ],
+    result: ["Any Stained Glass Pane", anyStainedGlassPane, 8],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Any Wool", anyWool],
+      ["Any Dye", anyDye],
+      false,
+
+      false,
+      false,
+      false,
+
+      false,
+      false,
+      false,
+    ],
+    result: ["Any Wool", anyWool],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Sand", sand],
+      ["Sand", sand],
+      ["Sand", sand],
+
+      ["Sand", sand],
+      ["Sand", sand],
+      ["Sand", sand],
+
+      ["Sand", sand],
+      ["Sand", sand],
+      ["Red Dye", redDye],
+    ],
+    result: ["Red Sand", redSand, 8],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Sandstone", sandstone],
+      ["Sandstone", sandstone],
+      ["Red Dye", redDye],
+
+      false,
+      false,
+      false,
+
+      false,
+      false,
+      false,
+    ],
+    result: ["Red Sandstone", redSandstone, 2],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Smooth Sandstone", smoothSandstone],
+      ["Smooth Sandstone", smoothSandstone],
+      ["Red Dye", redDye],
+
+      false,
+      false,
+      false,
+
+      false,
+      false,
+      false,
+    ],
+    result: ["Smooth Red Sandstone", smoothRedSandstone, 2],
+  },
+  {
+    type: "furnace",
+    input: ["Any Concrete Powder", anyConcretePowder],
+    result: ["Any Stained Glass", anyStainedGlass],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Iron Ingot", ironIngot],
+      ["String", string],
+
+      false,
+      ["Paper", paper],
+      ["Iron Ingot", ironIngot],
+
+      ["Paper", paper],
+      false,
+      false,
+    ],
+    result: ["Name Tag", nameTag],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Block of Gold", blockOfGold],
+      ["Block of Gold", blockOfGold],
+      ["Block of Gold", blockOfGold],
+
+      ["Block of Gold", blockOfGold],
+      ["Apple", apple],
+      ["Block of Gold", blockOfGold],
+
+      ["Block of Gold", blockOfGold],
+      ["Block of Gold", blockOfGold],
+      ["Block of Gold", blockOfGold],
+    ],
+    result: ["Enchanted Golden Apple", enchantedGoldenApple],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Blackstone", blackstone],
+      false,
+
+      false,
+      ["Blackstone", blackstone],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+    ],
+    result: ["Stone Sword", stoneSword],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+    ],
+    result: ["Stone Pickaxe", stonePickaxe],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      false,
+
+      ["Blackstone", blackstone],
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+    ],
+    result: ["Stone Axe", stoneAxe],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Blackstone", blackstone],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+    ],
+    result: ["Stone Shovel", stoneShovel],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+    ],
+    result: ["Stone Hoe", stoneHoe],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      false,
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+    ],
+    result: ["Furnace", furnace],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Oak Planks", oakPlanks],
+      ["Oak Planks", oakPlanks],
+      ["Oak Planks", oakPlanks],
+
+      ["Blackstone", blackstone],
+      ["Iron Ingot", ironIngot],
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      ["Redstone", redstone],
+      ["Blackstone", blackstone],
+    ],
+    result: ["Piston", piston],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      ["Redstone", redstone],
+      ["Redstone", redstone],
+      ["Iron Ingot", ironIngot],
+
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+    ],
+    result: ["Observer", observer],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      ["Blaze Rod", blazeRod],
+      false,
+
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      false,
+      false,
+      false,
+    ],
+    result: ["Brewing Stand", brewingStand],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      ["Bow", bow],
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      ["Redstone", redstone],
+      ["Blackstone", blackstone],
+    ],
+    result: ["Dispenser", dispenser],
+  },
+  {
+    type: "crafting",
+    input: [
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      false,
+      ["Blackstone", blackstone],
+
+      ["Blackstone", blackstone],
+      ["Redstone", redstone],
+      ["Blackstone", blackstone],
+    ],
+    result: ["Dropper", dropper],
+  },
+  {
+    type: "crafting",
+    input: [
+      false,
+      false,
+      false,
+
+      false,
+      ["Stick", stick],
+      false,
+
+      false,
+      ["Blackstone", blackstone],
+      false,
+    ],
+    result: ["Lever", lever],
   },
 ];
