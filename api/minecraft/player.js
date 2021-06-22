afa5b60bae6caaa685e2af883d374ebd65890cea
Angel <22824872+Zyplos@users.noreply.github.com>
2021-06-22 18:37:52 -0500

    hidden homes feature

--- api/minecraft/player.js
+++ api/minecraft/player.js
@@ -21,10 +21,19 @@ module.exports = async (req, res) => {
       connection
         .execute(
           // "SELECT BIN_TO_UUID(player_id) AS player_id, name, joined, community_id, home_x, home_y, home_z, BIN_TO_UUID(home_dimension) AS home_dimension FROM players WHERE player_id=UUID_TO_BIN(?)",
-          "SELECT BIN_TO_UUID(player_id) AS player_id, name, joined, community_id, home_x, home_y, home_z, BIN_TO_UUID(home_dimension) AS home_dimension FROM players WHERE name=?",
+          "SELECT BIN_TO_UUID(player_id) AS player_id, name, joined, community_id, home_x, home_y, home_z, BIN_TO_UUID(home_dimension) AS home_dimension, home_hidden FROM players WHERE name=?",
           [name]
         )
         .then(([rows, fields]) => {
+          if (rows.length > 0) {
+            const homeHidden = rows[0].home_hidden;
+            if (homeHidden) {
+              rows[0].home_x = null;
+              rows[0].home_y = null;
+              rows[0].home_z = null;
+              rows[0].home_dimension = "sneaky sneaky. nothing here";
+            }
+          }
           res.json({ data: rows });
         })
         .catch((error) => {
